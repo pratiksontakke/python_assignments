725c9c4cd6cb63563edc534da6f462abd545d51f
pratiksontakke <pratikass488@gmail.com>
2025-07-29 15:26:03 +0530

    Q15 Solved

--- intro_to_python_programming/Q15/modular_program_design.py
+++ intro_to_python_programming/Q15/modular_program_design.py
@@ -1,21 +1,42 @@
 class Library:
-    def __init__(self, books):
+    def __init__(self):
         self.books = set()
-    
+
     def add_book(self, book):
         self.books.add(book)
-        return f"Book {book} succesfully added."
-    
+        return f"Book '{book}' successfully added."
+
     def search_book(self, book):
         if book in self.books:
-            # return self.books.(book)
-            pass
-        pass
+            return f"Book '{book}' is available in the library."
+        else:
+            return f"Book '{book}' not found in the library."
 
-    def dispaly_books(self):
-        for book in self.books:
-            print(f"Book: {book}")
+    def display_books(self):
+        if self.books:
+            print("Books in the library:")
+            for book in self.books:
+                print(f"- {book}")
+        else:
+            print("No books available in the library.")
 
-
-    pass
-
+if __name__ == "__main__":
+    library = Library()
+    while True:
+        try:
+            user_input = int(input("\nType 1 to add a book\nType 2 to search for a book\nType 3 to view all books\nType 4 to exit\n>> "))
+            if user_input == 1:
+                book_name = input("Enter book name to add: ")
+                print(library.add_book(book_name))
+            elif user_input == 2:
+                book_name = input("Enter book name to search: ")
+                print(library.search_book(book_name))
+            elif user_input == 3:
+                library.display_books()
+            elif user_input == 4:
+                print("Exiting... Thank you!")
+                break
+            else:
+                print("Invalid input. Please choose between 1 and 4.")
+        except ValueError:
+            print("Please enter a valid number.")
